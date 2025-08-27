61bdc6ef4132f94f56011257d1864659b83c6c57
Peter V <vasilevskiipeter@gmail.com>
2025-08-27 18:26:31 +0400

    from python yt_dlp to cmd

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 echo "Installing started..."
-python3 -m pip install -r requirements
+apt install yt-dlp -y
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install -r requirements.txt
 echo "[TELEGRAM]" > config.ini
 echo "token = " >> config.ini
 echo "my_chat = " >> config.ini
-echo "Add telegram bot token and your chat id to config.ini"
+touch cookies.txt
+echo "Add telegram bot token and your chat id to config.ini and browser cookies to cookies.txt"
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from configparser import ConfigParser
 import telebot
-import yt_dlp
 import os
-
-
-URLS = ['https://www.youtube.com/watch?v=pvy9km7g6fw']
+import time
 
 
 CONFIG = ConfigParser()
@@ -19,43 +16,6 @@ except:
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
 
-class DownLogger:
-    def __init__(self, id, message_id):
-        self.chat_id = id
-        self.message_id = message_id
-
-    def debug(self, msg):
-        # For compatibility with youtube-dl, both debug and info are passed into debug
-        # You can distinguish them by the prefix '[debug] '
-        if msg.startswith('[debug] '):
-            pass
-        else:
-            self.info(msg)
-
-    def info(self, msg):
-        bot.edit_message_text(msg, self.chat_id, self.message_id)
-
-    def warning(self, msg):
-        pass
-
-    def error(self, msg):
-        print(msg)
-
-
-def download(url, mode, id, message_id=0):
-    ydl_opts = {
-        'logger': DownLogger(id, message_id),
-        'format': 'bestaudio/best',
-        'postprocessors': [{  # Extract audio using ffmpeg
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-        }]
-    }
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(url)
-
-
 @bot.message_handler(chat_id=[SECURE_ID], commands=['start'])
 def start_handler(message):
     bot.send_message(message.chat.id, "Send link to start downloading")
@@ -67,11 +27,20 @@ def url_handler(message):
     mes_id = bot.send_message(message.chat.id, "Got the link").message_id
 
     try:
-        download(message.text, "mp3", message.chat.id, mes_id)
+        os.popen(f'yt-dlp {message.text} -t mp3 --cookies cookies.txt > output.txt')
+        time.sleep(1)
+        new = ''
+        while 'Deleting' not in new:
+            last = new
+            new = open('output.txt', 'r').readlines()[-1]
+            if last != new:
+                bot.edit_message_text(new.strip(), message.chat.id, mes_id)
         bot.edit_message_text("Sending file...", message.chat.id, mes_id)
         filename = list(filter(lambda x: '.mp3' in x, os.listdir()))[0]
+
         with open(filename, 'rb') as f:
             bot.send_audio(message.chat.id, f)
+        
         os.remove(filename)
         
     except Exception as e:
